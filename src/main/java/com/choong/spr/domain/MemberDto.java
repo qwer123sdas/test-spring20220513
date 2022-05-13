b0823386ca22cd4c148f5d8aba6514a38be6903e
hyunsu <violin0117@naver.com>
2022-05-13 16:15:29 +0900

    first commit

---
+++ src/main/java/com/choong/spr/domain/MemberDto.java
@@ -0,0 +1,18 @@
+package com.choong.spr.domain;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+@Data
+public class MemberDto {
+	private int member_No;
+	private String member_ID;
+	private int member_PW;
+	private int member_Name;
+	private DateTimeFormat member_Date;
+	private String member_Sex;
+	private String member_Email;
+	private String member_Role;
+	private int member_KaKao;
+	
+}
